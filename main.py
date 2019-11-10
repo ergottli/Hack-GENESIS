b4a5f1de0d1856521ec85b7a0f36427eecd23372
Arseny <arseniy1098@yandex.ru>
2019-11-10 12:24:11 +0300

    Удалил код для второй программы распознавания. В png не конвертится.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from werkzeug.utils import secure_filename
 from flask import send_file
 import subprocess
+from PIL import Image
 import shutil
 
 app = Flask(__name__)
@@ -38,32 +39,23 @@ def download():
 
 
 def receive_recog_files(pathfile, filename):
-    molname_1 = os.path.splitext(filename)[0] + "_1.mol"
-    molname_2 =  os.path.splitext(filename)[0] + "_2.mol"
-    pngname_1 = os.path.splitext(filename)[0] + "_1.png"
-    smilesname_1 = os.path.splitext(filename)[0] + "_sm_1.txt"
+    molname = os.path.splitext(filename)[0] + "_1.mol"
+    pngname = os.path.splitext(filename)[0] + "_1.png"
+    smilesname = os.path.splitext(filename)[0] + "_sm_1.txt"
     # path's for mol files
-    molfile_1 = os.path.join(app.config["MOLECULES"], molname_1)
-    molfile_2 = os.path.join(app.config["MOLECULES"], molname_2)
-    pngfile_1 = os.path.join(app.config["MOLECULES"], pngname_1)
+    molfile = os.path.join(app.config["MOLECULES"], molname)
+    pngfile = os.path.join(app.config["MOLECULES"], pngname)
     # path's for smiles files
-    smiles_file_1 = os.path.join(app.config["MOLECULES"], smilesname_1)
+    smiles_file = os.path.join(app.config["MOLECULES"], smilesname)
     smiles_file_2 = os.path.join(app.config["MOLECULES"], os.path.splitext(filename)[0] + "_sm_2.txt")
     # recognize with imago, create mol and smiles files
-    subprocess.run(["./imago_console", pathfile, "-o", molfile_1], check=True)
-    subprocess.run(["chmod", "+x", molfile_1], check=True)
-    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), "-imol", molfile_1,
-         "-osmiles", "-O", smiles_file_1], check=True)
-    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), molfile_1,
-        "-O", pngfile_1], check=True)
-    print(molfile_1 + "\n" + pngfile_1)
-    # recognize with open-babel, create mol and smiles files TODO если не получится сконверить в png, удалить нахер кусок код и папку imagemagic
-    #subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), pathfile_png,
-    #     "-omol", "-O", molfile_2], check=True)
-    #subprocess.run(["chmod", "+x", molfile_2], check=True)
-    #subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), "-imol", molfile_2,
-    #     "-osmiles", "-O", smiles_file_2], check=True)
-    return molname_1, pngname_1, smilesname_1
+    subprocess.run(["./imago_console", pathfile, "-o", molfile], check=True)
+    subprocess.run(["chmod", "+x", molfile], check=True)
+    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), "-imol", molfile,
+         "-osmiles", "-O", smiles_file], check=True)
+    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "open-babel", "bin", "obabel"), molfile,
+        "-O", pngfile], check=True)
+    return molname, pngname, smiles_file
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -83,24 +75,19 @@ def upload_image():
                 filename = secure_filename(image.filename)
                 image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
                 pathfile = os.path.join(app.config["IMAGE_UPLOADS"], filename)
-                # TODO если не получится сконвертить в png, удалить этот кусок кода
-                #pathfile_png = os.path.join(app.config["IMAGE_UPLOADS"], os.path.splitext(filename)[0] + ".png")
-                #subprocess.run([os.path.join(cur_file, "imagemagic", "bin", "mogrify"), "-format", "jpg",pathfile, pathfile_png], check=True)
-                #print("pathfile_png " + pathfile_png)
-                #if os.path.exists(os.path.join(cur_file, os.path.splitext(filename)[0] + ".png")):
-                #    shutil.move(os.path.join(cur_file, os.path.join(cur_file, os.path.splitext(filename)[0] + ".png")), pathfile_png)
-                # png_f = [i for i in os.listdir(os.path.dirname(os.path.realpath(__file__))) if "png" in i]
-                # for i in png_f:
-                #     shutil.move(os.path.join(cur_file, i), os.path.join(app.config['MOLECULES'], i))
-                molname_1, pngname_1, smilesname_1 = receive_recog_files(pathfile, filename)
+                molname, pngname, smilesname = receive_recog_files(pathfile, filename)
                 print("Изображение сохранено")
-                # TODO Удаление загруженного файла
-                # os.remove(pathfile)
-                return render_template("viewer.html", molname=molname_1)
+                # return rec_menu(molname_1, pngname_1, smilesname_1)
+                return render_template("viewer.html", molname=molname)
             else:
                 print("Неверное расширение")
                 return render_template("upload_error.html")
     return render_template("index.html")
+
+
+@app.route("/rec_menu")
+def rec_menu(molname, pngname, smilesname):
+    return send_file(os.path.join(app.config["MOLECULES"], pngname))
 
 
 @app.route("/about")
